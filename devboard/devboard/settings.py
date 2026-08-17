735940fc243454fbf23ab5baa22edcd0a0980fb5
AlAmeen <niniolaalameen@gmail.com>
2026-08-17 15:35:27 +0100

    Fix production database configuration

--- devboard/devboard/settings.py
+++ devboard/devboard/settings.py
@@ -86,11 +86,20 @@ WSGI_APPLICATION = 'devboard.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
-}
+
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # ------------------------------------------------------------------
